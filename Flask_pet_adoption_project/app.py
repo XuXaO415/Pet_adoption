b7afe6738ba0d16553f537366c72313c644e6c42
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2021-09-14 21:15:13 -0700

    Actively working on this project

--- Flask_pet_adoption_project/app.py
+++ Flask_pet_adoption_project/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, render_template, redirect, flash, session
+from flask import Flask, request, render_template, redirect, flash, session, url_for
+from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Pet
 from forms import AddPetForm
+
 
 
 app = Flask(__name__)
@@ -14,9 +16,34 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
+
 connect_db(app)
 db.create_all()
 
-# @app.route('/')
-# def homepage():
-#     pet = Pet.query.all()
+@app.route("/")
+def list_pets():
+    """List pets"""
+    pets = Pet.query.all()
+    return render_template("list_pets.html", pets=pets)
+
+@app.route("/add", methods=["GET", "POST"])
+def add_pet():
+    """Add pet; handle form"""
+    form = AddPetForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.phot_url.data
+        age = form.age.data
+        notes = form.notes.data
+        
+        new_pet = Pet(name=name, species=species, photo_url=photo_url, age=age, notes=notes)
+        db.session(new_pet)
+        db.session.commit()
+        flash(f"{new_pet} added")
+        return render_template("pet_add_form.html", form=form)
+    
+    # else:
+    #     return render_template("pet_add_form.html", form=form )
+
+    
--- Flask_pet_adoption_project/forms.py
+++ Flask_pet_adoption_project/forms.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField, BooleanField, IntegerField, RadioField, SelectField
 from wtforms.validators import InputRequired, Email, Optional, URL, Optional
-
+from flask_sqlalchemy import SQLAlchemy
 
 
 class AddPetForm(FlaskForm):
-    name = StringField("Pet Name", validators=[InputRequired(message="Pet Name cannot be left blank")])
-    species = StringField("Species", validators=[InputRequired(message="Species field cannot be left blank")])
-    photo_url = StringField("Pet's Photo", validators=[Optional(), URL()])
-    age = FloatField("Enter Pet's age", validoators=[Optional()])
-    notes = StringField("Notes", validators=[Optional()])
+    name = StringField("Pet Name", validators=[InputRequired(message="Pet Name cannot be left blank")]),
+    species = StringField("Species", validators=[InputRequired(message="Species field cannot be left blank")]),
+    photo_url = StringField("Pet's Photo", validators=[Optional(), URL()]),
+    age = IntegerField("Enter Pet's age", validoators=[Optional()]),
+    notes = StringField("Notes", validators=[Optional()]),
     available = BooleanField("Pet is available", validators=[InputRequired(message="Pet is")])
     
 
