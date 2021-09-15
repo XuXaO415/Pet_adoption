5f719d055764a00489cdb2291cc4c464c6c9a672
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2021-09-14 18:08:12 -0700

    Added Pet adoption project setup

---
+++ Flask_pet_adoption_project/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Pet
+from forms import AddPetForm
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://adopt_db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "SuPpOrT_yOuR_lOcAl_ShElTeR"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
+
+# @app.route('/')
+# def homepage():
+#     form = 
---
+++ Flask_pet_adoption_project/tests.py
@@ -0,0 +1,2 @@
+# app.config['WTF_CSRF_ENABLED'] = False
+
