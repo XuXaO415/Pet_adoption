9b828091e2c80379e95c73da01d11e87057ae53f
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2021-09-20 21:23:47 -0700

    Added setup for Cupcake project

--- flask-cupcakes/app.py
+++ flask-cupcakes/app.py
@@ -1,1 +1,17 @@
 """Flask app for Cupcakes"""
+from flask import Flask, render_template, request, session, redirect, flash, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Cupcake
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcake_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "ThIsIsToTaLSeCrEt92761#"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
--- flask-cupcakes/models.py
+++ flask-cupcakes/models.py
@@ -1,1 +1,35 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+default_img ="https://tinyurl.com/demo-cupcake"
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+
+class Cupcake(db.Model):
+    """Cupcake model"""
+    
+    __tablename__ = "cupcakes"
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False, default=default_img)
+    
+    def serialize_cupcake(self):
+        """Serialize a cupcake SQLAlchemy obj to dict"""
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "image": self.image,
+        }
