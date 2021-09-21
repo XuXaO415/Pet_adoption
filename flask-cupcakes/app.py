e4b335ec271a5927a25f26994b18ab7587e9ab49
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2021-09-21 14:01:09 -0700

    Added content to app.py in preparation for Flask Cupcake project

--- flask-cupcakes/app.py
+++ flask-cupcakes/app.py
@@ -15,3 +15,31 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 connect_db(app)
+
+@app.route("/api/cupcakes")
+def list_cupcakes():
+    """Gather data and list all cupcakes"""
+    
+
+#  GET /cupcakes/[id] --> Get snack
+@app.route("/api/cupcakes/<int:cupcake_id>")
+def single_cupcake()s:
+    """Gather data on single cupcake"""
+    
+@app.route("/api/cupcakes", methods=["POST"])
+def custom_cupcake():
+    """Customize cupcake"""
+    
+    
+#############################################################
+#Part three: Update a& Delete Cupcakes
+
+# @app.route("/api/cupcakes/<int:cupcake_id>", methods=["PATCH"])
+# def update_cupcake():
+#     """Update cupcake"""
+    
+
+# @app.route("/api/cupcakes/<int:cupcake_id>", methods=["DELETE"])
+# def delete_cupcake():
+#     """"Delete cupcake or raise 404 if cupcake is not found"""
+    
